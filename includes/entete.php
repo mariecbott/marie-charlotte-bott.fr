b336dc97418cbd55af6c1d1795a50ae1153e4a5a
MC BOTT <143190516+mariecbott@users.noreply.github.com>
2023-08-28 16:21:28 +0200

    Add files via upload

---
+++ includes/entete.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style2.css" />
+        <link rel="shortcut icon" href="images/logo.ico" />
+        <title>MCB - Polyvalence</title>
+
+    </head>
+
+    <body id="top">
+
+
+        <div class="menu">
+
+            <h1><img src="images/logo/logo3.png" class="logomcb" width="60px" height="60px" />M-C BOTT</h1>
+
+            <label for="ch" id="lab"></label>
+            <input type="checkbox" id="ch" />
+            <nav>
+                <a href="index.php">ACCUEIL</a>
+                <a href="formation.php">FORMATION</a>
+                <a href="experience.php">EXPERIENCE</a>
+                <a href="skills.php">SKILLS</a>
+                <a href="contact.php">CONTACT</a>
+            </nav>
+
+        </div>
+
+        
---
+++ includes/pieddepage.php
@@ -0,0 +1,25 @@
+    <section class="fleche">    
+
+        <p><a href="#top"><img src="images/fleche.png" onmouseover="this.src='images/fleche2.png'" 
+        onmouseout="this.src='images/fleche.png'" alt="fleche" class="flechetop" /></a></p>
+
+    </section>
+
+    <footer>
+
+        <div class="footer2">
+
+            <p>Webmaster : Marie-Charlotte BOTT</p>
+            <p>Copyright 2022 - Tous Droits Réservés</p>
+            
+
+        </div>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
