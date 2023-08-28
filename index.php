47d58b0b6bff35c6ca1ad3ce60d3930d5f5243b4
MC BOTT <143190516+mariecbott@users.noreply.github.com>
2023-08-28 16:04:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php include('includes/entete.php'); ?>
+
+        <video autoplay loop muted>
+            <source src="video/polyvalence.mp4" type="video/mp4">
+        </video>
+
+    <section class="presentation">
+
+        <img src="images/logo/logo4.png" class="logomcb2" />
+        <h2>ADMINISTRATIVE, COMMERCIALE, WEBMASTER<br/>
+        Polyvalence est ce qui me définit!</h2>
+
+    </section>
+
+    <section id="quisuisje">
+
+            <article class="quisuisje">
+
+                <h3>A PROPOS DE MOI<br/>
+                    <img src="images/soulignement.png" class="soulignement" />
+                </h3>
+                
+
+                <p><img src="images/moi_mini.jpg" alt="moi" class="photomcb" />Bonjour, je suis Marie-Charlotte BOTT, j'ai 
+                39 ans. Je suis maman de 3 enfants, 2 filles et 1 garçon. Je suis née le 18 octobre 1983 à Saint-Pol sur Ternoise dans le 
+                Pas-de-Calais, petite ville où tout le monde se connaît.</p>
+
+                <p>J'ai obtenu mon <strong>Baccalauréat Sciences Techniques et Tertiaires</strong> option <strong>Action et Communication 
+                Administratives</strong> en 2003.</p>
+
+                <p>J'ai débuté ma carrière en tant qu'Assistante de Formation au sein du Centre Interrégional de Formation Professionnelle de 
+                l'Equipement en 2004. Depuis, j'ai pu exercer plusieurs variations du métier d'assistante administrative ainsi que du conseil clientèle  
+                au sein de différentes entreprises.</p>
+
+                <p>En parallèle, j'ai pu découvrir l'informatique à l'âge de 14 ans et je m'y suis prise de passion. J'ai découvert tout d'abord 
+                les différents logiciels tels que Lotus, puis Microsoft Office, Ciel... En 2010, j'ai pris connaissance du site siteduzero.com, 
+                ancien site d'OpenClassRooms. Et j'ai pu apprendre le HTML et le CSS et évolué avec ces langages.</p>
+
+                <p>En 2020, j'ai décidé de me former en autodidacte pendant mon temps libre. J'ai obtenu un Certificat de réussite dans mon 
+                apprentissage du HTML5 et du CSS3 et j'y ai acquis une certaine aisance. M'exerçant pendant mon temps libre, j'ai découvert d'autres 
+                langages tels que le PHP et le JAVASCRIPT. Depuis le 11 octobre 2021, j'ai intégré l'AFPA de Lille et sa nurserie numérique. Puis, dès 
+                le 8 novembre, j'ai intégré la formation de Développeur Full Stack au sein de l'AFPA de Roubaix.</p>
+
+                <p>Au fil de ma carrière professionnelle, je me suis rendue compte que l'administratif, le conseil clientèle et l'informatique 
+                pouvaient se combiner et me permettre d'acquérir de nouvelles compétences. Effectivement, j'ai pu gagner en efficacité, en 
+                professionnalisme et en compétences. D'où la réalisation de mon propre site afin de me démarquer et démontrer mon savoir-faire.</p>
+
+                <p><strong>Mes expériences au sein de différentes entreprises doivent être considérées comme un atout majeur. 
+                Effectivement, j'ai effectué volontairement plusieurs missions intérimaires et des remplacements pour maladie ou congé maternité. 
+                Cela m'a permis de m'adapter à tout milieu et à toute personne, d'où mon aisance relationnelle. Mon but étant désormais de me 
+                stabiliser au sein d'une entreprise et d'y apporter toutes mes compétences à long terme. </strong></p> 
+
+                <p>Mes atouts ? Je suis une personne généreuse, dynamique, autonome, aimant le travail en équipe.</p>
+
+                <p>Désormais, je suis devenue une personne entièrement polyvalente.</p>
+
+            </article> 
+
+        </section>
+
+        <h3 class="liensite"><a href="https://marie-charlotte-bott.fr/" class="liensite2">marie-charlotte-bott.fr</a></h3>
+
+        <section id="loisirs">
+
+            <article class="loisirs">   
+
+                <h3>LOISIRS<br/>
+                    <img src="images/soulignement2.png" class="soulignement" />
+                </h3>
+
+                <div class="blocloisirs1">
+
+                    <p><img src="images/loisirs/cinema2.png" onmouseover="this.src='images/loisirs/cinema.png'" onmouseout="this.
+                    src='images/loisirs/cinema2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">CINEMA</span><br/><br/>
+                    La science-fiction, le fantastique et l'horreur</p>
+
+                    <p><img src="images/loisirs/dessin2.png" onmouseover="this.src='images/loisirs/dessin.png'" onmouseout="this.
+                    src='images/loisirs/dessin2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">DESSIN</span><br/><br/>
+                    Surtout des portraits, ou paysages</p>
+
+                    <p><img src="images/loisirs/informatique2.png" onmouseover="this.src='images/loisirs/informatique.png'" onmouseout="this.
+                    src='images/loisirs/informatique2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">INFORMATIQUE</span><br/><br/>
+                    Création de sites web, dépannages</p>
+
+                </div>
+
+                <div class="blocloisirs2">
+
+                    <p><img src="images/loisirs/lecture2.png" onmouseover="this.src='images/loisirs/lecture.png'" onmouseout="this.
+                    src='images/loisirs/lecture2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">LECTURE</span><br/><br/>
+                    Les policiers, la science-fiction et l'horreur</p>
+
+                    <p><img src="images/loisirs/musique2.png" onmouseover="this.src='images/loisirs/musique.png'" onmouseout="this.
+                    src='images/loisirs/musique2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">MUSIQUE</span><br/><br/>
+                    Le vieux rock (70), la house...</p>
+
+                    <p><img src="images/loisirs/voyage2.png" onmouseover="this.src='images/loisirs/voyage.png'" onmouseout="this.
+                    src='images/loisirs/voyage2.png'" width="70px" height="70px" class="imgloisirs" />
+                    <span class="titreloisirs">VOYAGE</span><br/><br/>
+                    Découvrir d'autres horizons, d'autres façons de vivre</p>
+
+                </div>
+
+            </article>
+
+        </section>
+
+
+<?php include('includes/pieddepage.php'); ?>
